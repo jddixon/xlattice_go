5910ffc43d28454e8564932dfd2427c32e549a6d
Jim Dixon <jddixon@gmail.com>
2013-05-13 01:11:51 +0000

    adding Acceptor, Connector, Overlay, and Peer abstractions

---
+++ connector.go
@@ -0,0 +1,30 @@
+package xlattice_go
+
+// import java.io.IOException
+
+/**
+ * Used to establish a Connection with another entity (Node).
+ *
+ * The notion is that a node has a collection of Connectors used
+ * for establishing Connections with Peers, neighboring nodes.
+ *
+ * @author Jim Dixon
+ */
+type Connector interface {
+
+    /**
+     * Establish a Connection with another entity using the transport
+     * and address in the EndPoint.
+     * 
+     * @param nearEnd  local end point to use for connection
+     * @param blocking whether the new Connection is to be blocking
+     */
+    Connect (near EndPoint, blocking bool) (
+                        c Connection, e error) // throws IOException
+
+    /**
+     * @return the Acceptor EndPoint that this Connector is used to
+     *          establish connections to
+     */
+    GetFarEnd () EndPoint
+}
