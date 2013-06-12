d6f47d39531bc7855b8e6775154815011343ca32
Jim Dixon <jddixon@gmail.com>
2013-06-12 22:29:05 +0000

    benchmarks SimpleRNG against rand.Random

---
+++ rnglib/bench_test.go
@@ -0,0 +1,24 @@
+package rnglib
+
+import (
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func BenchmarkRand(b *testing.B) {
+	// typically 39.2 ns/op
+	for i := 0; i < b.N; i++ {
+		_ = rand.Int63() // Go's rand.Random package
+	}
+}
+
+func BenchmarkSimpleRNG(b *testing.B) {
+	t := time.Now().Unix()
+	rng := NewSimpleRNG(t)
+	b.ResetTimer()
+	// typically 28.4 ns/op
+	for i := 0; i < b.N; i++ {
+		_ = rng.Int63() // Mersenne Twister
+	}
+}
