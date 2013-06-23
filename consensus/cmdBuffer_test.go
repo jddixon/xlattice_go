6808fa4c8a52aa7e836365c3cd2020f2011c9d5d
Jim Dixon <jddixon@gmail.com>
2013-06-23 23:53:21 +0000

    noisy cmdBuffer_test.go succeeds

--- consensus/cmdBuffer_test.go
+++ consensus/cmdBuffer_test.go
@@ -2,7 +2,7 @@ package consensus
 
 import (
 	"container/heap"
-	// "fmt"
+	"fmt"
 	"github.com/jddixon/xlattice_go/rnglib"
 	. "launchpad.net/gocheck"
 	"testing"
@@ -59,4 +59,51 @@ func (s *XLSuite) TestCmdQ(c *C) {
 	// the length first.
 	//zzz		:= heap.Pop(&q)
 	// c.Assert(zzz, Equals, nil)
+}
+func (s *XLSuite) TestCmdBuffer(c *C) {
+	var pairMap = map[int64]string{
+		1: "foo",
+		2: "bar",
+		3: "baz",
+		4: "it's me!",
+		5: "my chance will come soon",
+		6: "it's my turn now",
+		7: "wait for me",
+	}
+	order := [...]int{1, 2, 3, 6, 6, 5, 4, 1, 7}
+	var buf CmdBuffer
+	p := &buf
+	var out = make(chan CmdPair, len(order)+1) // must exceed len(order)
+	var stopCh = make(chan bool, 1)
+	p.Init(out, stopCh, 0)               // was Init()
+	c.Assert(p.Running(), Equals, false) // HACK - was false
+
+	go p.Run()
+
+	for !p.Running() {
+		fmt.Print(".")
+		time.Sleep(100 * time.Millisecond)
+	}
+	fmt.Println("")
+	c.Assert(p.Running(), Equals, true)
+
+	fmt.Println("sending pairs")
+	for n := 0; n < len(order); n++ {
+		cmd := pairMap[int64(n)]
+		pair := CmdPair{Seqn: int64(order[n]), Cmd: cmd}
+		p.InCh <- pair
+	}
+
+	time.Sleep(time.Millisecond)
+	fmt.Println("collecting results")
+	var results [7]CmdPair
+	for n := 0; n < 7; n++ {
+		results[n] = <-out
+		c.Assert(results[n].Seqn, Equals, int64(n+1))
+		fmt.Printf("GOT RESULT %v\n", n+1)
+	}
+
+	time.Sleep(time.Millisecond)
+	stopCh <- true
+	fmt.Println("stop has been sent")
 }
