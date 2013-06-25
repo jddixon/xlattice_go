cd46325f2f23ed8e2ce2d87cc19618131f9db565
Jim Dixon <jddixon@gmail.com>
2013-06-25 21:31:19 +0000

    added Len() with mutex

--- consensus/cmdBuffer.go
+++ consensus/cmdBuffer.go
@@ -28,35 +28,49 @@ type cmdPlus struct {
 }
 
 type pairQ []*cmdPlus
+var qSy    sync.RWMutex
 
 func (q pairQ) Len() int { // not in heap interface
-	return len(q)
+	// Q length is volatile, so we lock, copy, unlock, return the copy.
+	// Expected use is in testing.
+	qSy.RLock()
+	length := len(q)
+	qSy.RUnlock()
+	return length
 }
 
 // implementation of the heap interface /////////////////////////////
 // These functions are invoke like heap.Push(&q, &whatever)
 
 func (q pairQ) Less(i, j int) bool {
-	return q[i].pair.Seqn < q[j].pair.Seqn
+	qSy.RLock()
+	whether := q[i].pair.Seqn < q[j].pair.Seqn
+	qSy.RUnlock()
+	return whether
 }
 
 func (q pairQ) Swap(i, j int) {
+	qSy.Lock()
 	q[i], q[j] = q[j], q[i]
 	q[i].index = i // remember this is post-swap
 	q[j].index = j
+	qSy.Unlock()
 }
 
 func (q *pairQ) Push(x interface{}) {
+	qSy.Lock()
 	n := len(*q)
 	thePair := x.(*cmdPlus) // a cast
 	thePair.index = n
 	*q = append(*q, thePair)
+	qSy.Unlock()
 }
 
 /////////////////////////////////////////////////////////////////////
 // XXX if the length is zero, we get a panic in the heap code. //////
 /////////////////////////////////////////////////////////////////////
 func (q *pairQ) Pop() interface{} {
+	qSy.Lock()
 	nowQ := *q
 	n := len(nowQ)
 	if n == 0 {
@@ -65,6 +79,7 @@ func (q *pairQ) Pop() interface{} {
 	lastPair := nowQ[n-1] // last element
 	lastPair.index = -1   // doesn't matter
 	*q = nowQ[0 : n-1]
+	qSy.Unlock()
 	return lastPair
 }
 
@@ -86,6 +101,10 @@ func (c *CmdBuffer) Init(out chan NumberedCmd, StopCh chan bool, lastSeqn int64,
 	c.lastSeqn = lastSeqn
 }
 
+// This is synched at the q level.
+func (c *CmdBuffer) Len() int {
+	return c.q.Len()
+}
 func (c *CmdBuffer) Running() bool {
 	// c.running is volatile, so we lock, copy, unlock, return the copy
 	c.sy.Lock()
@@ -124,7 +143,7 @@ func (c *CmdBuffer) Run() {
 				for c.q.Len() > 0 {
 					first := c.q[0]
 					if first.pair.Seqn <= c.lastSeqn {
-						//	fmt.Printf("        Q: DISCARDING %v, DUPE\n",
+						//	// fmt.Printf("        Q: DISCARDING %v, DUPE\n",
 						//							first.pair.Seqn)
 						// a duplicate, so discard
 						_ = heap.Pop(&c.q).(*cmdPlus)
@@ -132,16 +151,16 @@ func (c *CmdBuffer) Run() {
 						pp := heap.Pop(&c.q).(*cmdPlus)
 						c.OutCh <- *pp.pair
 						c.lastSeqn += 1
-						//	fmt.Printf("        Q: SENT %v\n", c.lastSeqn)
+						//	// fmt.Printf("        Q: SENT %v\n", c.lastSeqn)
 					} else {
-						//	fmt.Printf("        Q: LEAVING %v IN Q\n",
+						//	// fmt.Printf("        Q: LEAVING %v IN Q\n",
 						//							first.pair.Seqn)
 						break
 					}
 				}
 			} else {
 				// seqN > c.lastSeqn + 1, so buffer
-				//	fmt.Printf("    HIGH SEQN %v, SO BUFFERING\n", seqN)
+				// fmt.Printf("    HIGH SEQN %v, SO BUFFERING\n", seqN)
 				pp := &cmdPlus{pair: &inPair}
 				heap.Push(&c.q, pp)
 			}
@@ -149,7 +168,7 @@ func (c *CmdBuffer) Run() {
 			c.sy.Lock()
 			c.running = false
 			c.sy.Unlock()
-			//	fmt.Println("c.running has been set to false")
+			// fmt.Println("c.running has been set to false")
 		}
 	}
 }
