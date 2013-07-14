e15f79f14e22c6a06be17c80b1a4a8314718bf97
Jim Dixon <jddixon@gmail.com>
2013-07-14 00:22:54 +0000

    Documenting u256x256

--- gocheck_test.go
+++ gocheck_test.go
@@ -8,6 +8,7 @@ import (
 // IF USING gocheck, need a file like this in each package=directory.
 
 func Test(t *testing.T) { TestingT(t) }
-type XLSuite struct{}
-var _ = Suite(&XLSuite{})
 
+type XLSuite struct{}
+
+var _ = Suite(&XLSuite{})
