e93e982b6e2599f8c6ef27147cb96815c19f1dae
Jim Dixon <jddixon@gmail.com>
2013-09-06 14:35:42 -0700

    reg.go New() complete, untested

--- reg/reg.go
+++ reg/reg.go
@@ -3,9 +3,13 @@ package reg
 // xlattice_go/reg/reg.go
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
 	"fmt"
 	xm "github.com/jddixon/xlattice_go/msg"
 	xn "github.com/jddixon/xlattice_go/node"
+	xi "github.com/jddixon/xlattice_go/nodeID"
+	xo "github.com/jddixon/xlattice_go/overlay"
 	xt "github.com/jddixon/xlattice_go/transport"
 )
 
@@ -19,40 +23,68 @@ const (
 )
 
 type RegNode struct {
-	Acc       *xt.TcpAcceptor
-	StopCh    chan bool
-	StoppedCh chan bool
+	Acc          *xt.TcpAcceptor
+	StopCh       chan bool
+	StoppedCh    chan bool
+	privCommsKey *rsa.PrivateKey
+	privSigKey   *rsa.PrivateKey
 	xn.Node
 }
 
 // options normally set from the command line
 type RegOptions struct {
 	Lfs     string
+	Address string
 	Port    int
 	Testing bool
 	Verbose bool
 }
 
-// Create a registry node around a live node which has an open acceptor
+// Create a registry node.
 
-func New(n *xn.Node, stopCh, stoppedCh chan bool) (rn *RegNode, err error) {
+func New(name, lfs string, id *xi.NodeID,
+	cKey, sKey *rsa.PrivateKey,
+	overlay *xo.OverlayI, endPoint xt.EndPointI,
+) (rn *RegNode, err error) {
 
-	if n == nil {
-		err = xm.NilNode
+	var acc xt.AcceptorI
+	var n *xn.Node
+
+	if name == "" {
+		name = "xlReg"
 	}
-	tcpAcc := n.GetAcceptor(0).(*xt.TcpAcceptor)
-	if err == nil && tcpAcc == nil {
-		err = xm.AcceptorNotLive
+	if lfs == "" {
+		lfs = "/var/app/xlReg"
 	}
-	if err == nil && stopCh == nil {
-		err = xm.NilControlCh
+	if err == nil && id == nil {
+		id, err = xi.New(nil)
+	}
+	if err == nil && cKey == nil {
+		cKey, err = rsa.GenerateKey(rand.Reader, 2048)
+	}
+	if err == nil && sKey == nil {
+		sKey, err = rsa.GenerateKey(rand.Reader, 2048)
 	}
 	if err == nil {
+		endPoints := []xt.EndPointI{endPoint}
+		n, err = xn.New(name, id, lfs, cKey, sKey, nil, endPoints, nil)
+	}
+	if err == nil {
+		acc = n.GetAcceptor(0) // XXX should be open
+		if acc == nil {
+			err = xm.AcceptorNotLive
+		}
+	}
+	if err == nil {
+		stopCh := make(chan bool, 1)
+		stoppedCh := make(chan bool, 1)
 		rn = &RegNode{
-			Acc:       tcpAcc,
-			StopCh:    stopCh,
-			StoppedCh: stoppedCh,
-			Node:      *n,
+			Acc:          acc.(*xt.TcpAcceptor),
+			StopCh:       stopCh,
+			StoppedCh:    stoppedCh,
+			privCommsKey: cKey,
+			privSigKey:   sKey,
+			Node:         *n,
 		}
 	}
 	return
