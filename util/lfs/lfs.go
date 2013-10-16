68c3f7fd5014e794d2fe3b2154e1ef2d48d761de
Jim Dixon <jddixon@gmail.com>
2013-10-16 13:29:28 -0700

    adding MkdirsToFile(path, perm)

--- util/lfs/lfs.go
+++ util/lfs/lfs.go
@@ -4,6 +4,7 @@ package lfs
 
 import (
 	"os"
+	"strings"
 )
 
 // If the directory named does not exist, create it, restricting
@@ -11,8 +12,26 @@ import (
 // "lfs", that is, ./lfs/
 
 func CheckLFS(lfs string) (err error) {
+	// XXX should verify that LFS is a directory
 	if lfs == "" {
 		lfs = "lfs"
 	}
 	return os.MkdirAll(lfs, 0700)
+}
+
+// Given a path to a file, create any missing intermediate directories.
+func MkdirsToFile(pathToFile string, perm os.FileMode) (err error) {
+
+	parts := strings.Split(pathToFile, "/")
+	if len(parts) > 1 {
+		var pathToDir string
+		if len(parts) == 2 {
+			// just drop the file name
+			pathToDir = parts[0]
+		} else {
+			pathToDir = strings.Join( parts[:len(parts)-1], "/")
+		}
+		err = os.MkdirAll(pathToDir, perm)
+	}
+	return
 }
