9e53f2d042a44f9fe4cfca5d4915a8923b5f095d
Jim Dixon <jddixon@gmail.com>
2013-11-08 17:28:41 -0800

    replacing util.SameBytes with bytes.Equal

--- util/same_bytes.go
+++ util/same_bytes.go
@@ -3,37 +3,39 @@ package util
 // xlattice_go/util/same_bytes.go
 
 import (
-	"unsafe"
+	"bytes"
+	//"unsafe"
 )
 
 // Return true if the two byte slices have the same length and no
 // byte differ.
 
 func SameBytes(a, b []byte) bool {
-	var aInt, bInt int
-	if len(a) != len(b) {
-		return false
-	}
-	length := len(a)
-	if length == 0 {
-		return true
-	}
-	sizeInt := int(unsafe.Sizeof(aInt))
-	var i int
-	for i := 0; i < length-sizeInt; i += sizeInt {
-		aInt = *(*int)(unsafe.Pointer(&a[i]))
-		bInt = *(*int)(unsafe.Pointer(&b[i]))
-		if aInt != bInt {
-			return false
-		}
-	}
-	// check for leftover bytes
-	if i > length {
-		for i -= sizeInt; i < len(a); i++ {
-			if a[i] != b[i] {
-				return false
-			}
-		}
-	}
-	return true
+	// var aInt, bInt int
+	// if len(a) != len(b) {
+	// 	return false
+	// }
+	// length := len(a)
+	// if length == 0 {
+	// 	return true
+	// }
+	// sizeInt := int(unsafe.Sizeof(aInt))
+	// var i int
+	// for i := 0; i < length-sizeInt; i += sizeInt {
+	// 	aInt = *(*int)(unsafe.Pointer(&a[i]))
+	// 	bInt = *(*int)(unsafe.Pointer(&b[i]))
+	// 	if aInt != bInt {
+	// 		return false
+	// 	}
+	// }
+	// // check for leftover bytes
+	// if i > length {
+	// 	for i -= sizeInt; i < len(a); i++ {
+	// 		if a[i] != b[i] {
+	// 			return false
+	// 		}
+	// 	}
+	// }
+	// return true
+	return bytes.Equal(a, b)
 }
