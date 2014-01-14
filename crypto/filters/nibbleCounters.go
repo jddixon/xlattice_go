23fdfc3758fa43303ea453a2dfe8f8b1c18cc848
Jim Dixon <jddixon@gmail.com>
2014-01-13 16:13:46 -0800

    added nibbleCounters*.go for CountingBloom

---
+++ crypto/filters/nibbleCounters.go
@@ -0,0 +1,68 @@
+package filters
+
+// xlattice_go/crypto/filters/nibbleCounters.go
+
+const NIBBLE_MASK = ^uint(0xf)
+
+/**
+ * As it stands, this class is not thread-safe.  Using classes are
+ * expected to provide synchronization.
+ *
+ * @author Jim Dixon
+ */
+type NibbleCounters struct {
+	counters []uint // short in Java
+}
+
+func NewNibbleCounters(filterInts uint) *NibbleCounters {
+	return &NibbleCounters{
+		counters: make([]uint, filterInts*8),
+	}
+}
+
+func (nc *NibbleCounters) Clear() {
+	for i := 0; i < len(nc.counters); i++ {
+		nc.counters[i] = 0
+	}
+}
+
+/**
+ * Increment the nibble, ignoring any overflow
+ * @param filterWord offset of 32-bit word
+ * @param filterBit  offset of bit in that word (so in range 0..31)
+ * @return value of nibble after operation
+ */
+func (nc *NibbleCounters) Inc(filterWord uint, filterBit uint) uint {
+	counterShort := 8*filterWord + filterBit/4
+	counterCell := filterBit % 4
+	shiftBy := counterCell * 4
+	cellValue := 0xf & (nc.counters[counterShort] >> shiftBy)
+	if cellValue < 15 {
+		cellValue++
+	}
+	// mask off the nibble and then OR new value in
+	// TABLE LOOKUP MORE EFFICIENT
+	nc.counters[counterShort] ^= (NIBBLE_MASK << shiftBy)
+	nc.counters[counterShort] |= (cellValue << shiftBy)
+	return cellValue
+}
+
+/**
+ * Decrement the nibble, ignoring any overflow
+ * @param filterWord offset of 32-bit word
+ * @param filterBit  offset of bit in that word (so in range 0..31)
+ * @return value of nibble after operation
+ */
+func (nc *NibbleCounters) Dec(filterWord uint, filterBit uint) uint {
+	counterShort := 8*filterWord + filterBit/4
+	counterCell := filterBit % 4
+	shiftBy := counterCell * 4
+	cellValue := 0xf & (nc.counters[counterShort] >> shiftBy)
+	if cellValue > 0 {
+		cellValue--
+	}
+	// mask off the nibble and then OR new value in
+	nc.counters[counterShort] ^= (NIBBLE_MASK << shiftBy)
+	nc.counters[counterShort] |= (cellValue << shiftBy)
+	return cellValue
+}
---
+++ crypto/filters/nibbleCounters_test.go
@@ -0,0 +1,69 @@
+package filters
+
+// xlattice_go/crypto/filters/nibbleCounters_test.go
+
+import (
+	"fmt" // DEBUG
+	//xr "github.com/jddixon/xlattice_go/rnglib"
+	. "launchpad.net/gocheck"
+)
+
+var _ = fmt.Print
+
+/**
+ * Tests the counters associated with Bloom filters for sets whose members
+ * are 20-byte SHA1 digests.
+ */
+func (s *XLSuite) doTestBit(c *C, nibCount *NibbleCounters,
+	filterWord uint, filterBit uint) {
+
+	for i := uint(0); i < 18; i++ {
+		value := nibCount.Inc(filterWord, filterBit)
+		// DEBUG
+		fmt.Printf("filterword %d, filterBit %d, i %d ==> value %d\n",
+			filterWord, filterBit, i, value)
+		// END
+		if i < 15 {
+			c.Assert(value, Equals, i+1)
+		} else {
+			c.Assert(value, Equals, 15)
+		}
+	}
+	for i := uint(0); i < 18; i++ {
+		value := nibCount.Dec(filterWord, filterBit)
+		if i < 15 {
+			c.Assert(value, Equals, 14-i)
+		} else {
+			c.Assert(value, Equals, 0)
+		}
+	}
+}
+func (s *XLSuite) doTestWord(c *C, nibCount *NibbleCounters, filterWord uint) {
+	// test the low order bit, the high order bit, and a few from
+	// the middle
+	s.doTestBit(c, nibCount, filterWord, 0)
+	s.doTestBit(c, nibCount, filterWord, 1)
+	s.doTestBit(c, nibCount, filterWord, 14)
+	s.doTestBit(c, nibCount, filterWord, 15)
+	s.doTestBit(c, nibCount, filterWord, 16)
+	s.doTestBit(c, nibCount, filterWord, 31)
+
+}
+func (s *XLSuite) doTest(c *C, m uint) {
+
+	filterInts := uint(1<<m) / 32 // 2^m bits fit into this many ints
+	// test the low order word, the high order word, and a few from
+	// the middle
+	nibCount := NewNibbleCounters(filterInts)
+	s.doTestWord(c, nibCount, 0)
+	s.doTestWord(c, nibCount, (filterInts/2)-1)
+	s.doTestWord(c, nibCount, (filterInts / 2))
+	s.doTestWord(c, nibCount, (filterInts/2)+1)
+	s.doTestWord(c, nibCount, filterInts-1)
+}
+func (s *XLSuite) TestNibs(c *C) {
+	s.doTest(c, 12)
+	s.doTest(c, 13)
+	s.doTest(c, 14)
+	s.doTest(c, 20)
+}
