5518c4fe00036e2ffa8a607e636e30e043e996c6
Jim Dixon <jddixon@gmail.com>
2014-02-13 19:41:07 -0800

    adding protocol/aes_cnx*, various TODOs

---
+++ protocol/aes_cnx/aes_cnx.go
@@ -0,0 +1,107 @@
+package aes_cnx
+
+// xlattice_go/reg/aes_cnx.go
+
+// Assume that a generator for this code is parameterized by
+//	package name	- defaults to using local directory name
+//  protocol name	- defaults to using whatever precedes "Msg" in *.proto
+//  MSG_BUF_LEN		- defaults to 16 (K assumed)
+//  file name		- defaults to protocol name + "_aes_cnx.go"
+//  struct name		- defaults to protocol name + "AesCnxHandler"
+//
+// Generator is tested by generating the text for xlattice_go/reg
+// and then comparing it to this file, with this comment block dropped.
+
+import (
+	"code.google.com/p/goprotobuf/proto"
+	"crypto/aes"
+	"crypto/cipher"
+	xc "github.com/jddixon/xlattice_go/crypto"
+	xt "github.com/jddixon/xlattice_go/transport"
+	// "sync"
+)
+
+const (
+	MSG_BUF_LEN = 16 * 1024
+)
+
+type AesCnxHandler struct {
+	State                              int
+	Cnx                                *xt.TcpConnection
+	engine                             cipher.Block
+	encrypter                          cipher.BlockMode
+	decrypter                          cipher.BlockMode
+	iv1, key1, iv2, key2, salt1, salt2 []byte
+}
+
+func (a *AesCnxHandler) SetupSessionKey() (err error) {
+	a.engine, err = aes.NewCipher(a.key2)
+	if err == nil {
+		a.encrypter = cipher.NewCBCEncrypter(a.engine, a.iv2)
+		a.decrypter = cipher.NewCBCDecrypter(a.engine, a.iv2)
+	}
+	return
+}
+
+// Read data from the connection.  XXX This will not handle partial
+// reads correctly.
+//
+func (a *AesCnxHandler) ReadData() (data []byte, err error) {
+	data = make([]byte, MSG_BUF_LEN)
+	count, err := a.Cnx.Read(data)
+	if err == nil && count > 0 {
+		data = data[:count]
+		return
+	}
+	return nil, err
+}
+
+func (a *AesCnxHandler) WriteData(data []byte) (err error) {
+	count, err := a.Cnx.Write(data)
+
+	// XXX handle cases where not all bytes written
+
+	_ = count
+	return
+}
+func DecodePacket(data []byte) (*XLRegMsg, error) {
+	var m XLRegMsg
+	err := proto.Unmarshal(data, &m)
+	// XXX do some filtering, eg for nil op
+	return &m, err
+}
+
+func EncodePacket(msg *XLRegMsg) (data []byte, err error) {
+	return proto.Marshal(msg)
+}
+
+func EncodePadEncrypt(msg *XLRegMsg, engine cipher.BlockMode) (
+	ciphertext []byte, err error) {
+
+	var paddedData []byte
+
+	cData, err := EncodePacket(msg)
+	if err == nil {
+		paddedData, err = xc.AddPKCS7Padding(cData, aes.BlockSize)
+	}
+	if err == nil {
+		msgLen := len(paddedData)
+		nBlocks := (msgLen + aes.BlockSize - 2) / aes.BlockSize
+		ciphertext = make([]byte, nBlocks*aes.BlockSize)
+		engine.CryptBlocks(ciphertext, paddedData) // dest <- src
+	}
+	return
+}
+
+func DecryptUnpadDecode(ciphertext []byte, engine cipher.BlockMode) (
+	msg *XLRegMsg, err error) {
+
+	plaintext := make([]byte, len(ciphertext))
+	engine.CryptBlocks(plaintext, ciphertext) // dest <- src
+
+	unpaddedCData, err := xc.StripPKCS7Padding(plaintext, aes.BlockSize)
+	if err == nil {
+		msg, err = DecodePacket(unpaddedCData)
+	}
+	return
+}
---
+++ protocol/aes_cnx/gocheck_test.go
@@ -0,0 +1,144 @@
+package aes_cnx
+
+import (
+	//"crypto/rand"
+	//"crypto/rsa"
+	//"encoding/hex"
+	//"fmt"
+	//xn "github.com/jddixon/xlattice_go/node"
+	//xi "github.com/jddixon/xlattice_go/nodeID"
+	//xr "github.com/jddixon/xlattice_go/rnglib"
+	//xt "github.com/jddixon/xlattice_go/transport"
+	. "launchpad.net/gocheck"
+	//"strings"
+	"testing"
+)
+
+// IF USING gocheck, need a file like this in each package=directory.
+func Test(t *testing.T) { TestingT(t) }
+
+type XLSuite struct{}
+
+var _ = Suite(&XLSuite{})
+
+const (
+	VERBOSITY = 1
+)
+
+//func (s *XLSuite) makeAnID(c *C, rng *xr.PRNG) (id []byte) {
+//	id = make([]byte, SHA3_LEN)
+//	rng.NextBytes(&id)
+//	return
+//}
+//func (s *XLSuite) makeANodeID(c *C, rng *xr.PRNG) (nodeID *xi.NodeID) {
+//	id := s.makeAnID(c, rng)
+//	nodeID, err := xi.New(id)
+//	c.Assert(err, IsNil)
+//	c.Assert(nodeID, Not(IsNil))
+//	return
+//}
+//func (s *XLSuite) makeAnRSAKey(c *C) (key *rsa.PrivateKey) {
+//	key, err := rsa.GenerateKey(rand.Reader, 2048)
+//	c.Assert(err, IsNil)
+//	c.Assert(key, Not(IsNil))
+//	return key
+//}
+//
+//// Creates a local (127.0.0.1) endPoint and adds it to the node.
+//// XXX This code was hacked from ../node/node_test.go.
+//
+//func (s *XLSuite) makeALocalEndPoint(c *C, node *xn.Node) {
+//	addr := fmt.Sprintf("127.0.0.1:0")
+//	ep, err := xt.NewTcpEndPoint(addr)
+//	c.Assert(err, IsNil)
+//	c.Assert(ep, Not(IsNil))
+//	ndx, err := node.AddEndPoint(ep)
+//	c.Assert(err, IsNil)
+//	c.Assert(ndx, Equals, 0) // it's the only one
+//}
+//
+//// Return an initialized and tested host, with a NodeID, commsKey,
+//// and sigKey.   XXX This code was hacked from ../node/node_test.go
+//// and then simplified a bit.
+//
+//func (s *XLSuite) makeHostAndKeys(c *C, rng *xr.PRNG) (
+//	n *xn.Node, ckPriv, skPriv *rsa.PrivateKey) {
+//
+//	// XXX names may not be unique
+//	name := rng.NextFileName(6)
+//	for {
+//		first := string(name[0])
+//		if !strings.Contains(first, "0123456789") &&
+//			!strings.Contains(name, "-") {
+//			break
+//		}
+//		name = rng.NextFileName(6)
+//	}
+//	id := s.makeANodeID(c, rng)
+//	lfs := "tmp/" + hex.EncodeToString(id.Value())
+//	ckPriv = s.makeAnRSAKey(c)
+//	skPriv = s.makeAnRSAKey(c)
+//
+//	n, err2 := xn.New(name, id, lfs, ckPriv, skPriv, nil, nil, nil)
+//
+//	c.Assert(err2, IsNil)
+//	c.Assert(n, Not(IsNil))
+//	c.Assert(name, Equals, n.GetName())
+//	actualID := n.GetNodeID()
+//	c.Assert(true, Equals, id.Equal(actualID))
+//	// s.doKeyTests(c, n, rng)
+//	c.Assert(0, Equals, (*n).SizePeers())
+//	c.Assert(0, Equals, (*n).SizeOverlays())
+//	c.Assert(0, Equals, n.SizeConnections())
+//	c.Assert(lfs, Equals, n.GetLFS())
+//	return n, ckPriv, skPriv
+//}
+//
+//// Using functions must check to ensure members have unique names
+//
+//func (s *XLSuite) makeAMemberInfo(c *C, rng *xr.PRNG) *MemberInfo {
+//	attrs := uint64(rng.Int63())
+//	bn, err := xn.NewBaseNode(
+//		rng.NextFileName(8),
+//		s.makeANodeID(c, rng),
+//		&s.makeAnRSAKey(c).PublicKey,
+//		&s.makeAnRSAKey(c).PublicKey,
+//		nil) // overlays
+//	c.Assert(err, IsNil)
+//	return &MemberInfo{
+//		Attrs:    attrs,
+//		BaseNode: *bn,
+//	}
+//}
+//
+//// Make a RegCluster for test purposes.  Cluster member names are guaranteed
+//// to be unique but the name of the cluster itself may not be.
+//
+//func (s *XLSuite) makeACluster(c *C, rng *xr.PRNG, epCount, size uint) (
+//	rc *RegCluster) {
+//
+//	var err error
+//	c.Assert(1 < size && size <= 64, Equals, true)
+//
+//	attrs := uint64(rng.Int63())
+//	name := rng.NextFileName(8) // no guarantee of uniqueness
+//	id := s.makeANodeID(c, rng)
+//
+//	rc, err = NewRegCluster(name, id, attrs, size, epCount)
+//	c.Assert(err, IsNil)
+//
+//	for count := uint(0); count < size; count++ {
+//		cm := s.makeAMemberInfo(c, rng)
+//		for {
+//			if _, ok := rc.MembersByName[cm.GetName()]; ok {
+//				// name is in use, so try again
+//				cm = s.makeAMemberInfo(c, rng)
+//			} else {
+//				err = rc.AddMember(cm)
+//				c.Assert(err, IsNil)
+//				break
+//			}
+//		}
+//	}
+//	return
+//}
